df7d64e3741ddfc3d18c3c1de87d0df7c5f67d53
Christian Parker <christianparker512@gmail.com>
2021-06-05 18:07:18 -0500

    deleting backwards

--- gobackwards.py
+++ gobackwards.py
@@ -1,7 +1,10 @@
-data = [104, 101, 4, 105, 308, 103, 5,
-, 306, 106, 102, 108]
+data = [104, 101, 4, 105, 308, 103, 5, 306, 106, 102, 108]
 min_valid = 100
 max_valid = 200
 
 for index in range(len(data) -1, -1, - 1):
-    print(index)
+    if data[index]<min_valid or data[index]>max_valid:
+        print(index,data)
+        del data[index]
+
+print(index)
