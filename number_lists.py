b7a7ecbe4df4ee458e4fe15b82b051a610fba790
Christian Parker <christianparker512@gmail.com>
2021-06-05 15:19:16 -0500

    combining two lists using concatenations and .sort method

--- number_lists.py
+++ number_lists.py
@@ -1,26 +1,10 @@
+empty_list = []
+
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print (max(even))
-print(min(odd))
-print(max(odd))
+numbers = even + odd
+print(numbers)
 
-print()
-print(len(even))
-print(len(odd))
-
-print()
-print("mississippi".count("s"))
-
-even.extend(odd)
-print(even)
-another_even = even
-print(even)
-
-even.sort(reverse=True)
-print(even)
-
-even.sort()
-print(even)
-
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
