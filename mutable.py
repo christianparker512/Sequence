6c03ff0973645dc915949d0b182dce4a4a2614a7
Christian Parker <christianparker512@gmail.com>
2021-06-05 11:33:39 -0500

    chaining assignments

--- mutable.py
+++ mutable.py
@@ -14,3 +14,8 @@ print(id(another_list))
 
 shopping_list +=["brownies"]
 print(shopping_list)
+print(id(shopping_list))
+print(another_list)
+
+#chaining assignments
+a = b = c = d = e = f = another_list
