155e3b36f6150903a1b714da44a6f8b8d20b3797
Christian Parker <christianparker512@gmail.com>
2021-06-05 15:43:48 -0500

    replacing third position with trackball

--- lists_intro.py
+++ lists_intro.py
@@ -4,11 +4,18 @@ computer_parts = ["computer",
                   "mouse",
                   "mouse mat"
                   ]
-for part in computer_parts:
-    print(part)
+#for part in computer_parts:
+#    print(part)
 
 print()
-print(computer_parts[2])
+#print(computer_parts[2])
 
-print(computer_parts[0:3])
-print(computer_parts[-1])
+#print(computer_parts[0:3])
+#print(computer_parts[-1])
+
+print(computer_parts)
+computer_parts[3] = "trackball"
+#printing items from position three
+print(computer_parts[3:])
+
+computer_parts[3:] = ["trackball"]
