7aeef01186136baf5fb1056f470d476247d495ce
Christian Parker <christianparker512@gmail.com>
2021-06-05 14:14:19 -0500

    removing from list

--- buy_computer.py
+++ buy_computer.py
@@ -2,42 +2,34 @@ available_parts = ["computer",
                    "monitor",
                    "keyboard",
                    "mouse",
-                   "mouse pad",
                    "hdmi cable",
                    "dvd drive"
                    ]
 #valid_choices = [str(i) for i in range(1, len(available_parts) +1)]
 valid_choices = []
-for i in range(1,len(available_parts) + 1):
+for i in range(1, len(available_parts) + 1):
     valid_choices.append(str(i))
 print(valid_choices)
-current_choice = "_"
-computer_parts = [] #create an empty list
+current_choice = "-"
+computer_parts = [] # create an empty list
+
 while current_choice != '0':
     if current_choice in valid_choices:
-        print("Adding {}".format(current_choice))
-        index = int(current_choice) -1
+        index = int(current_choice) - 1
         chosen_part = available_parts[index]
-        computer_parts.append(chosen_part)
-        if current_choice == '1':
-            computer_parts.append("computer")
-        elif current_choice == '2':
-            computer_parts.append("keyboard")
-        elif current_choice == '3':
-            computer_parts.append("monitor")
-        elif current_choice == '4':
-            computer_parts.append("mouse")
-        elif current_choice == '5':
-            computer_parts.append("mouse pad")
-        elif current_choice == '6':
-            computer_parts.append("hdmi cable")
-
+        if chosen_part in computer_parts:
+            # it's already in, so remove it
+            print("Removing {}".format(current_choice))
+            computer_parts.remove(chosen_part)
+        else:
+            print("Adding {}".format(current_choice))
+            computer_parts.append(chosen_part)
+        print("Your list now contains: {}".format(computer_parts))
     else:
-        print("Please add options from the list below: ")
+        print("Please add options from the list below:")
         for number, part in enumerate(available_parts):
             print("{0}: {1}".format(number + 1, part))
 
     current_choice = input()
 
 print(computer_parts)
-
