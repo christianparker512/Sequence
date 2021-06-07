3cfe0ee3502d3cccab4857a7262dee118e1ce437
Christian Parker <christianparker512@gmail.com>
2021-06-07 14:04:09 -0500

    shortening the code and including a conditional to break the code

--- jukebox_menu.py
+++ jukebox_menu.py
@@ -1,14 +1,18 @@
 from nested_data import albums
 
+SONGS_LIST_INDEX = 3
+
 while True:
     print("Please choose your album (invalid choice exits):")
     for index, (title, artist, year, songs) in enumerate(albums):
-        print("{}: {}, {}, {}, {}"
-              .format(index + 1, title, artist, year, songs))
+        print("{}: {}".format(index + 1, title))
 
-    for index, value in enumerate(albums):
-        title, artist, year, songs = value
-        print(index, title, artist, year, songs)
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice -1][SONGS_LIST_INDEX]
+    else:
+        break
 
-    break
-
+    print(albums[choice - 1])
+    print(songs_list)
+    print()
