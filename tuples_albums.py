8539d56cf13413f4b103e19b72f7bd81ce1b2196
Christian Parker <christianparker512@gmail.com>
2021-06-07 12:35:55 -0500

    challenge solution

--- tuples_albums.py
+++ tuples_albums.py
@@ -1,27 +1,17 @@
-albums = [("Welcome to my Nightmare", "alice cooper", 1975),
-          ("bad company", "bad company", 1974),
-           ("Nightflight", "budgie", 1981),
-            ("more mayhem", "emilda may", 2011),
-             ("ride the lightning", "metallica", 1984)
-]
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
+
 print(len(albums))
 
-for album in albums:
+for name, artist, year in albums:
     print("Album: {}, Artist: {}, Year: {}"
-          .format(album[0], album[1], album[2]))
-#print(metallica[0])
-#print(metallica[1])
-#print(metallica[2])
+          .format(name, artist, year))
 
-#metallica2 = list(metallica)
-#print(metallica2)
-
-#metallica2[0] = "Master of Puppets"
-#print(metallica2)
-
-table = ("Coffee table", 200, 100, 75, 34.50)
-
-print(table[1] * table[2])
-
-name, length, width, height, price = table
-print(length * width)
+for album in albums:
+    name, artist, year = album
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
