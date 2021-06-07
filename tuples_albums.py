bd2b535d7a2a488d04910a0739e1288eb609bfa5
Christian Parker <christianparker512@gmail.com>
2021-06-07 11:41:31 -0500

    tuples

---
+++ tuples_albums.py
@@ -0,0 +1,16 @@
+welcome = "Welcome to my Nightmare", "alice cooper", 1975
+bad = "bad company", "bad company", 1974
+budgie = "Nightflight", "budgie", 1981
+imelda = "more mayhem", "emilda may", 2011
+metallica = "ride the lightning", "metallica", 1984
+print(metallica)
+
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+metallica2 = list(metallica)
+print(metallica2)
+
+metallica2[0] = "Master of Puppets"
+print(metallica2)
