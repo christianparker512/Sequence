0a359878613aa16ca6d24bf569c3223ad4e7d2d5
Christian Parker <christianparker512@gmail.com>
2021-06-07 12:35:04 -0500

    unpacking tuples using the album

--- tuples_albums.py
+++ tuples_albums.py
@@ -1,19 +1,23 @@
-welcome = "Welcome to my Nightmare", "alice cooper", 1975
-bad = "bad company", "bad company", 1974
-budgie = "Nightflight", "budgie", 1981
-imelda = "more mayhem", "emilda may", 2011
-metallica = "ride the lightning", "metallica", 1984
-print(metallica)
+albums = [("Welcome to my Nightmare", "alice cooper", 1975),
+          ("bad company", "bad company", 1974),
+           ("Nightflight", "budgie", 1981),
+            ("more mayhem", "emilda may", 2011),
+             ("ride the lightning", "metallica", 1984)
+]
+print(len(albums))
 
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+for album in albums:
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(album[0], album[1], album[2]))
+#print(metallica[0])
+#print(metallica[1])
+#print(metallica[2])
 
-metallica2 = list(metallica)
-print(metallica2)
+#metallica2 = list(metallica)
+#print(metallica2)
 
-metallica2[0] = "Master of Puppets"
-print(metallica2)
+#metallica2[0] = "Master of Puppets"
+#print(metallica2)
 
 table = ("Coffee table", 200, 100, 75, 34.50)
 
